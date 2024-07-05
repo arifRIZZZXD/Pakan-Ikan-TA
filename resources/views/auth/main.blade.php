9932a481cd2609b33f323faa40eb7cbd692ab071
Syarif Hidayatullah <syarifhdyt009xd@gmail.com>
2024-07-05 18:23:36 +0700

    cxcx

--- resources/views/auth/main.blade.php
+++ resources/views/auth/main.blade.php
@@ -102,6 +102,7 @@
         </script>
     @endif
 
+    
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
   </body>
